004b736fb286f63e0c11c3bb88b20b98d876a961
raghavanixdhruv <103164394+raghavanixdhruv@users.noreply.github.com>
2022-10-14 20:40:57 +0530

    Create t.py

---
+++ t.py
@@ -0,0 +1,9 @@
+name=input("enter name:")
+bod=input("enter bod:")
+number=input("enter number:")
+email=input("enter email:")
+
+print("your name is:",name)
+print("your bod is:",bod)
+print("your number is:",number)
+print("your email is:", email+ "@gmail.com")
