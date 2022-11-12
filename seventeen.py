8276f00370a33b7074d2f57bbb5cc4c2ca3e2fd6
raghavanixdhruv <103164394+raghavanixdhruv@users.noreply.github.com>
2022-11-12 14:51:00 +0530

    Update seventeen.py

--- seventeen.py
+++ seventeen.py
@@ -1,6 +1,5 @@
-year=int(input("enter year:"))
-if(year % 4 != 0):
-    print("its leap year");
+year = int(input("enter year"))
+if((year % 4 == 0) or (year % 100 != 0) and (year % 4 == 0)):
+    print ("it is a leap year")
 else:
-    print("its not leap year");
-    
+    print ("it is not a leap year")
