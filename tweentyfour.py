e368fe53701bee4433d85a45f5cb84c78cf74f5a
raghavanixdhruv <103164394+raghavanixdhruv@users.noreply.github.com>
2022-11-17 11:36:23 +0530

    Add files via upload

---
+++ tweentyfour.py
@@ -0,0 +1,12 @@
+
+num=int(input("enter the number of element number:"))
+l1=0
+for i in range(num):
+    x=int(input('enter number:'))
+    l1+=x
+
+print('total:',l1)
+avg=l1/num
+print('average is :',avg)
+
+
